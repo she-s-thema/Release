25074447a603dbe8fc6caa28b817753a688eb52d
parkdaxun <parkde0207@gmail.com>
2022-07-25 18:59:17 +0900

    [feat] Login

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -13,10 +13,19 @@ const AuthStack = () => {
             screenOptions={{
                 headerTitleAlign : 'center',
                 cardStyle : { backgroundColor : theme.background },
+                headerTintColor: theme.headerTintColor,
             }}
         >
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Signup" component={Signup} />
+            <Stack.Screen
+                name="Login"
+                component={Login}
+                options={{ headerShown : false }}
+            />
+            <Stack.Screen
+                name="Signup"
+                component={Signup}
+                options={{ headerBackTitleVisible : false }}
+            />
         </Stack.Navigator>
     );
 };
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,17 +1,20 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { TouchableWithoutFeedback, Keyboard } from "react-native";
 import styled from 'styled-components/native';
-import {Image, Input} from '../components';
+import {Button, Image, Input} from '../components';
 import { images } from '../utils/images';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { validateEmail, removeWhitespace } from "../utils/common";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Container = styled.View`
   flex : 1;
   justify-content: center;
   align-items : center;
   background-color: ${({ theme }) => theme.background};
-  padding : 20px;
+  padding : 0 20px;
+  padding-top : ${({ insets : { top } }) => top}px;
+  padding-bottom : ${({ insets : {bottom}}) => bottom}px;
 `;
 
 const ErrorText = styled.Text`
@@ -24,10 +27,17 @@ const ErrorText = styled.Text`
 `;
 
 const Login = ({navigation}) => {
+    const insets = useSafeAreaInsets();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const passwordRef = useRef();
     const [errorMessage, setErrorMessage] = useState('');
+    const _handleLoginButtonPress = () => {};
+    const [disabled, setDisabled] = useState(true);
+
+    useEffect(() => {
+        setDisabled(!(email && password && !errorMessage));
+    }, [email, password, errorMessage]);
 
     const _handleEmailChange = email => {
         const changedEmail = removeWhitespace(email);
@@ -45,7 +55,7 @@ const Login = ({navigation}) => {
             contentContainerStyle={{ flex : 1 }}
             extraScrollHeight={20}
         >
-            <Container>
+            <Container insets={insets}>
                 <Image url={images.logo} imageStyle={{ borderRadius : 8 }}/>
                 <Input
                     label = "Email"
@@ -60,12 +70,22 @@ const Login = ({navigation}) => {
                     label="Password"
                     value={password}
                     onChangeText={_handlePasswordChange}
-                    onSubmitEditing={() => {}}
+                    onSubmitEditing={_handleLoginButtonPress}
                     placeholder="Password"
                     returnKeyType="done"
                     isPassword
                 />
                 <ErrorText>{errorMessage}</ErrorText>
+                <Button
+                    title="Login"
+                    onPress={_handleLoginButtonPress}
+                    disabled={disabled}
+                />
+                <Button
+                    title="Sign up with email"
+                    onPress={() => navigation.navigate('Signup')}
+                    isFilled={false}
+                />
             </Container>
         </KeyboardAwareScrollView>
     );
